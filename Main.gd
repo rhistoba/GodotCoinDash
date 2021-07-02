699996d5d179f8f60bdc663f48e5bc430a8823db
rhistoba <fujitakeisound@gmail.com>
2021-07-02 21:43:00 +0900

    implement gameplay

--- Main.gd
+++ Main.gd
@@ -14,7 +14,6 @@ func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
 
 func new_game():
 	playing = true
@@ -25,6 +24,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 func spawn_coins():
 	for i in range(4 + level):
@@ -33,8 +34,30 @@ func spawn_coins():
 		c.screensize = screensize
 		c.position = Vector2(rand_range(0, screensize.x), rand_range(0, screensize.y))
 
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
+
 func _process(delta):
 	if playing and $CoinContainer.get_child_count() == 0:
 		level += 1
 		time_left += 5
 		spawn_coins()
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left < 0:
+		game_over()
+
+
+func _on_Player_hurt():
+	game_over()
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
